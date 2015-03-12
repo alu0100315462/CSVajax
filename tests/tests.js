1f5fb2b84aeee84d5365bc394e97a4b5e4700ba3
alu0100315462 <alu0100315462@ull.edu.es>
2015-03-12 10:35:42 +0000

    un solo test prueba

---
+++ tests/tests.js
@@ -0,0 +1,16 @@
+var assert = chai.assert;
+
+suite('CSV', function() {
+    setup(function(){
+      if (typeof __html__ !== 'undefined') {
+          document.body.innerHTML = __html__['tests/index.html'];
+          original = document.getElementById('original');
+          converted = document.getElementById('converted');
+      }
+    });
+    test('Test de tabla con un solo un elemento', function() {
+        original.value = "prueba1;
+        calculate();
+        assert.deepEqual(tabla.innerHTML, '<table class="table table-bordered">\n<tbody><tr><td>1prueba1</td></tr>\n</tbody></table>')
+    });
+});
