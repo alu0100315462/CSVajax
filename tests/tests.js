c7aa292a27adaa8d7e3018db84d2405e854379aa
alu0100315462 <alu0100315462@ull.edu.es>
2015-03-12 21:31:09 +0000

    arreglando test

--- tests/tests.js
+++ tests/tests.js
@@ -17,7 +17,7 @@ suite('CSV', function() {
     test('Test con varios elementos, comillas escapadas y comas entre comillas', function() {
         original.value = 'el libro de O\"Reilly,el numero 7,2,"el numero 7,2","frase con , comas, en medio"';
         calculate();
-        assert.deepEqual(finaltable.innerHTML, '<p><table class="center" id="result"><tbody><tr><td>el libro de O"Reilly</td><td>el numero 7</td><td>2</td><td>el numero 7,2</td><td>frase con , comas, en medio</td></tr></tbody></table></p>')
+        assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>el libro de O"Reilly</td>                                  <td>el numero 7</td>                                  <td>2</td>                                  <td>el numero 7,2</td>                                  <td>frase con , comas, en medio</td>              </tr>\n</tbody></table>')
     });
 
 });
