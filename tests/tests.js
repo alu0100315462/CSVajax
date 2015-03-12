b2d3ed37942710a9b5115943ac1b4d3d21b0a2c2
alu0100315462 <alu0100315462@ull.edu.es>
2015-03-12 23:17:05 +0000

    test linea correcto

--- tests/tests.js
+++ tests/tests.js
@@ -13,17 +13,17 @@ suite('CSV', function() {
         calculate();
         assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>prueba1</td>              </tr>\n</tbody></table>')
     });
-
-    test('Test con varios elementos, comillas escapadas y comas entre comillas', function() {
-        original.value = 'el libro de O\"Reilly,el numero 7,2,"el numero 7,2","frase con , comas, en medio"';
-        calculate();
-        assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>el libro de O"Reilly</td>                                  <td>el numero 7</td>                                  <td>2</td>                                  <td>el numero 7,2</td>                                  <td>frase con , comas, en medio</td>              </tr>\n</tbody></table>')
-    });
     
     test('Prueba en diferentes lineas', function() {
         original.value = 'prueba, en\ndiferentes\nlineas, "con , coma"';
         calculate();
         assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>prueba</td>                                  <td> en</td>              </tr>\n<tr class="error">                    <td>diferentes</td>              </tr>\n<tr>                    <td>lineas</td>                                  <td>con , coma</td>              </tr>\n</tbody></table>')
+    });
+
+    test('Test con varios elementos, comillas escapadas y comas entre comillas', function() {
+        original.value = 'el libro de O\"Reilly,el numero 7,2,"el numero 7,2","frase con , comas, en medio"';
+        calculate();
+        assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>el libro de O"Reilly</td>                                  <td>el numero 7</td>                                  <td>2</td>                                  <td>el numero 7,2</td>                                  <td>frase con , comas, en medio</td>              </tr>\n</tbody></table>')
     });
 
    test('Prueba almacenamiento web local', function(){
